437d3c83dea8268d6ccb8de00acb3224ff559fd7
Mrunalikale21 <mrunalikale76@gmail.com>
2024-06-19 12:50:02 +0530

    commit

--- dist/sample.js
+++ dist/sample.js
@@ -1,1 +1,16 @@
 "use strict";
+//type asseration to acess dom element
+const btn = document.getElementById("btn");
+// we can use like this also
+// 1)const btn = <HTMLElement>document.getElementById("btn") as HTMLElement;
+//or
+// 2)const btn = document.getElementById("btn") !;
+btn.onclick;
+const img = document.getElementById("myImg"); //here we specifing about html element
+img.src;
+const form = document.getElementById("myform");
+const myinput = document.querySelector("form > input");
+form.onsubmit = (e) => {
+    e.preventDefault();
+    console.log(myinput.value);
+};
